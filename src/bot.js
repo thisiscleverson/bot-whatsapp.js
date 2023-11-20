623a824451d8aa074aac454473105768f4b8a19c
cleverson <cleverson2020333@gmail.com>
2023-11-20 11:28:42 -0300

    refactore bot and config json

--- src/bot.js
+++ src/bot.js
@@ -1,62 +1,81 @@
-const Phrases    = require('../phraselist.json') // ler o arquivo json com todas a frases para analisar
-const qrcode     = require('qrcode-terminal') 
-const { Client, Status } = require('whatsapp-web.js')
-const client     = new Client
+const configBot  = require('../config.json') 
+const QRcode     = require('qrcode-terminal') 
+const { Client } = require('whatsapp-web.js')
+const { readFileSync, existsSync, writeFile} = require('fs')
+const client     = new Client({ puppeteer: { headless: true,args: ['--no-sandbox', '--disable-setuid-sandbox']} });
 
+
+
+const historyFilePath = './history.txt'
 
 //functions
+
+/*
 const getHours = () => {
     let currentTime = new Date()
     return `${currentTime.getHours()}:${currentTime.getMinutes()}:${currentTime.getSeconds()}`
 }
+*/
 
 
-const checkMessage = (Msg, contact) => {
-    let msg = Msg.normalize('NFD').replace(/[\u0300-\u036f]/g, "").toUpperCase()
+const checkMessage = (mensagem) => {
+    const messageSeatsRemoved = mensagem.normalize('NFD').replace(/[\u0300-\u036f]/g, "").toUpperCase()
 
-    for(let index in Phrases['phrases']){
-        //console.log(msg,':', msg.indexOf(Phrases['phrases'][index]))
-        if (msg.indexOf(Phrases['phrases'][index]) != -1) {
-            console.log(" ")    
-           	console.log(`📩 mensagem [ ${getHours()} ] por ${contact}: ${Msg}`)
-		    console.log("🔎 palavra chave: "+Phrases['phrases'][index])
-		return true
-        }
-    }
-    
-    return false
-}   
+    const results = configBot['keyword'].some((keyword) => messageSeatsRemoved.indexOf(keyword) != -1)
 
-
-const answers = () => {
-    let index = Math.floor(Math.random() * (Phrases['answers'].length - 1) + 1) // sortear qual frase de resposta vai ser usada
-    return Phrases['answers'][index]   
+    return results
 }
 
 
-// functions client 
+const RandomAnswers = () => {
+    let index = Math.floor(Math.random() * (configBot['answers'].length - 1) + 1) 
+    return configBot['answers'][index]   
+}
+
+
+const saveHistory = (contents) => {
+    let contentsFile = ''
+    if(existsSync(historyFilePath)){
+        try {
+            contentsFile = readFileSync(historyFilePath, 'utf8')
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    const newContents = contentsFile + '\n' + contents
+    writeFile(historyFilePath, newContents, (error) => console.error(error))
+}
+
+
 client.on('qr', qr => {
-   qrcode.generate(qr, {small: true}) // mostra o codigo QR para inicializar o bot
+   console.clear()
+   QRcode.generate(qr, {small: true}) // criar um QR code no terminal
 })
 
 
 client.on('ready', () => {
-    console.log('> Anton pronto para ação ✅ ')
+    console.clear()
+    console.log('> Connected ✅ ')
 })
 
 
 client.on('message', async message => {
-    let contact = await message.getContact()
-    if(checkMessage(message.body, contact.pushname) == true){
-        try{
-            await message.reply(answers())
-
-        }catch(err){
-            console.log(`> ❗ O Anton teve um ploblema a responder ${contact.pushname} as [ ${getHours()} ]`)
-            console.log(`> 📩 mensagem recebida: ${message.body}`)
+    message.getContact().then((contact) => {
+        if(!message.isStatus && checkMessage(message.body) == true){
+            try{
+                message.reply(RandomAnswers())
+                let log = `by: ${contact.name}: ${message.body}`
+                saveHistory(log)
+                console.log(log)
+            }catch(err){
+                console.log(`> Ocorreu um problema a responder ${contact.name} as [ ${getHours()} ]`)
+                console.log(`> A mensagem recebida foi: ${message.body}`)
+            }
         }
-    }
+    })
 })
+
 
 client.initialize()
 
